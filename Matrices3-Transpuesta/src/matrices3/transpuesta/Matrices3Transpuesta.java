032f2a24874b1f851c1cf7f08d23aef19d924a61
Erick Galileo Padilla <erick.galileo@outlook.com>
2022-10-18 16:11:40 -0600

    enunciado

---
+++ Matrices3-Transpuesta/src/matrices3/transpuesta/Matrices3Transpuesta.java
@@ -0,0 +1,22 @@
+package matrices3.transpuesta;
+
+/**
+ 
+  @author Galileo :B
+  
+   Elaborar un programa de Java with Ant usando matrices el cual devuelva la 
+   transpuesta de una matriz  que llenaremos de valores automaticamente.
+   Ejemplo: 
+
+    1 2 3   |    1 4 7 
+    4 5 6   |    2 5 8
+    7 8 9   |    3 6 9
+    
+ **/
+public class Matrices3Transpuesta {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
